3394e7358c77e1cba810864177382d626b773eac
yukuda <leeyookoo@naver.com>
2023-01-23 14:13:57 +0900

    BoardController

--- src/main/java/com/example/demo/controller/BoardController.java
+++ src/main/java/com/example/demo/controller/BoardController.java
@@ -1,15 +1,32 @@
 package com.example.demo.controller;
 
+
+import com.example.demo.domain.entity.Board;
+
 import com.example.demo.dto.BoardDto;
 import com.example.demo.service.BoardService;
+
+import org.springframework.data.domain.Page;
+
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 
-import java.util.List;
-// controller test
+
+
+
+
 @Controller
 public class BoardController {
+
+
     private BoardService boardService;
 
     public BoardController(BoardService boardService) {
@@ -17,15 +34,20 @@ public class BoardController {
     }
 
     @GetMapping("/")
-    public String list(Model model) {
-        List<BoardDto> boardDtoList = boardService.getBoardList();
-        model.addAttribute("postList", boardDtoList);
-        return "board/list.html";
+    public String list(Model model, @PageableDefault(size = 2 , sort = "id", direction = Sort.Direction.ASC)
+    Pageable pageable) {
+        Page<Board> postList = boardService.pageList(pageable);
+        int startPage = Math.max(1, postList.getPageable().getPageNumber() - 4);
+        int endPage = Math.min(postList.getTotalPages(), postList.getPageable().getPageNumber() + 4);
+        model.addAttribute("startPage", startPage);
+        model.addAttribute("endPage", endPage);
+        model.addAttribute("postList", postList);
+        return "board/list";
     }
 
     @GetMapping("/post")
     public String post() {
-        return "board/post.html";
+        return "board/post";
     }
 
     @PostMapping("/post")
@@ -38,14 +60,14 @@ public class BoardController {
     public String detail(@PathVariable("id") Long id, Model model) {
         BoardDto boardDto = boardService.getPost(id);
         model.addAttribute("post", boardDto);
-        return "board/detail.html";
+        return "board/detail";
     }
 
     @GetMapping("/post/edit/{id}")
     public String edit(@PathVariable("id") Long id, Model model) {
         BoardDto boardDto = boardService.getPost(id);
         model.addAttribute("post", boardDto);
-        return "board/edit.html";
+        return "board/edit";
     }
 
     @PutMapping("/post/edit/{id}")
@@ -59,4 +81,11 @@ public class BoardController {
         boardService.deletePost(id);
         return "redirect:/";
     }
+
+
+
+
+
+
+
 }

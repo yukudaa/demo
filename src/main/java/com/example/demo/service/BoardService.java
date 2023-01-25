3ca2c2844878500f060f04492871e4778927faa3
yukuda <leeyookoo@naver.com>
2023-01-25 12:56:46 +0900

    BoardService

--- src/main/java/com/example/demo/service/BoardService.java
+++ src/main/java/com/example/demo/service/BoardService.java
@@ -12,11 +12,15 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Service
 
@@ -65,5 +69,17 @@ public class BoardService {
 		boardRepository.deleteById(id);
 	}
 
+	/* 회원가입 시, 유효성 체크 */
+	@Transactional(readOnly = true)
+	public Map<String, String> validateHandling(Errors errors) {
+		Map<String, String> validatorResult = new HashMap<>();
+
+		/* 유효성 검사에 실패한 필드 목록을 받음 */
+		for (FieldError error : errors.getFieldErrors()) {
+			String validKeyName = String.format("valid_%s", error.getField());
+			validatorResult.put(validKeyName, error.getDefaultMessage());
+		}
+		return validatorResult;
+	}
 }
 

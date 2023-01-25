5dc4ab223788ab013527dec28dc476daa2ab6de4
yukuda <leeyookoo@naver.com>
2023-01-25 12:54:48 +0900

    Board

--- src/main/java/com/example/demo/domain/entity/Board.java
+++ src/main/java/com/example/demo/domain/entity/Board.java
@@ -9,6 +9,8 @@ import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+
 import java.time.LocalDateTime;
 
 @Getter
